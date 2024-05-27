3118b4e14a40fd0460ec3892a2a030da1debfd34
SaiCharan-ABNTech <154917195+SaiCharan-ABNTech@users.noreply.github.com>
2024-05-27 21:04:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "foo" {
   ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
+  key_name = "windows"
   tags = {
       Name = "TF-Instance"
   }
